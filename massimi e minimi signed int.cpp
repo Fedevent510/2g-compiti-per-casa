9164c5ebf0f929e2605d906cbe0d92d1a4f0abb3
Fedevent510 <fedevent2001@yahoo.it>
2017-04-21 23:54:06 +0200

    Add files via upload

---
+++ massimi e minimi signed int.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+//signed int non ho ben capito se si debba scrivere int main() o signed int main ()
+
+{
+char max = 32,767;
+char min = -32,768;
+
+
+cout << "Ecco il massimo: " << max << endl;
+cout << "Ecco il minimo: " << min << endl;
+cout << "Ecco il massimo -1: " <<  max-1 << endl;
+cout << "ecco il minimo -1: " << min-1 << endl;
+
+ return 0;
+ }
