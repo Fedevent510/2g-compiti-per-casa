9f82b35ed11c278d4d069b0a39f96ace33e2d634
Fedevent510 <fedevent2001@yahoo.it>
2017-04-21 23:45:07 +0200

    Add files via upload

---
+++ massimi e minimi char.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+char min = 127;
+char max = -128;
+
+
+cout << "Ecco il massimo: " << max << endl;
+cout << "Ecco il minimo: " << min << endl;
+cout << "Ecco il massimo -1: " <<  max-1 << endl;
+cout << "ecco il minimo -1: " << min-1 << endl;
+
+ return 0;
+ }
