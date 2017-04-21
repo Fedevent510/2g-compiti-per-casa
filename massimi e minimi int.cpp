ccefb08093c7530009b59eb392f63eb8f2ee7800
Fedevent510 <fedevent2001@yahoo.it>
2017-04-21 23:45:22 +0200

    Add files via upload

---
+++ massimi e minimi int.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+int a;
+
+a = 4294967296-1;
+
+cout << "La variabile a vale" << a << endl;
+
+if(a < 4294967296-1)
+
+cout<< "Testo vero";
+
+
+else
+    cout << "test falso";
+
+ return 0;
+ }
